21ff50e1771e8d7d34d11cf8de485388972cba00
Ljubiša Moćić <ljubisa.mocic@gmail.com>
2018-09-11 18:14:51 +0200

    Lambda pre-deploy

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ app.get('/:name', function (req, res) {
 	});
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
